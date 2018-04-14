ffdf5205c09078c8ec50d2b8a2966eb31c154ba6
Pascal Rhod <pascal.rhod@canaltp.fr>
2018-04-14 23:11:11 +0200

    update tyr action list

--- assets/js/tyr.js
+++ assets/js/tyr.js
@@ -1,53 +1,43 @@
-
-function print_tyr_job_list(response){
-    selected_region=""
-    var now = new Date();
-    str="<table border='1px' >";
-    str+="<tr><th>id</th><th>instance</th><th>created_at</th><th>updated_at</th><th>state</th><th>data_sets</th><th>params</th></tr>";
-    for (var i in response.jobs){
-        job=response.jobs[i];
-        if ((job.state == "pending") && (job.data_sets.length == 0) ) {continue;}
-        //var myDate = r.end_production_date?IsoToJsDate(r.end_production_date):now;
-        //alert(myDate);
-        str+="<tr>";
-        str+="<td>" + job.id+"</td>";
-        str+="<td>" + job.instance.name+"</td>";
-        d = new Date(job.created_at);
-        d = new Date(d.getTime() + 3600*1000);
-        str+="<td>" + formatDate(d, "yyyy-mm-dd") + "<br>" + formatDate(d, "hh:nn:ss") + "</td>";
-        if (job.updated_at) {
-            d = new Date(job.updated_at);
-            d = new Date(d.getTime() + 3600*1000);
-        } else d="null";
-        str+="<td>" + formatDate(d, "yyyy-mm-dd") + "<br>" + formatDate(d, "hh:nn:ss") + "</td>";
-        str+="<td>" + job.state+"</td>";
-        if (job.data_sets.length > 0) {
-            str+="<td>";
-            for (ds in job.data_sets) {
-                str += job.data_sets[ds].type + " - " + job.data_sets[ds].name + "<br>";
-            }
-            str+="</td>";
-        } else {
-            str+="<td>&nbsp;</td>";
-        }
-        str_properties="";
-        for (p in job.instance) {
-            if (p !="id" && p!= "name")
-                str_properties += p + ":" + job.instance[p]+"<br>";
-        }
-        str+="<td>" + str_properties+"</td>";
-        str+="</tr>";
-    }
-    str+="</table>"
-    document.getElementById('div_tyr_job_list').innerHTML=str;
-}
-
-
-function tyr_onLoad() {
+$(document).ready(function() {
     menu.show_menu("menu_div");
 
     t=extractUrlParams();
-    callTyrJS(t["ws_name"], "jobs/"+t["coverage"], print_tyr_job_list);
+    callTyrJS(t["ws_name"], "jobs/"+t["coverage"], display_tyr_job_list);
+} );
+
+
+function display_tyr_job_list(response){
+    var to_display = [];
+    for (var i in response.jobs){
+        var job = response.jobs[i];
+        if ((job.state == "pending") && (job.data_sets.length == 0) ) {continue;}
+        for (var j in job.data_sets) {
+            var ds = job.data_sets[j];
+            var obj = {};
+            obj.id = job.id;
+            obj.name = job.instance.name;
+            obj.created_at = moment.tz(job.created_at, "Africa/Abidjan").tz("Europe/Paris").format("YYYY-MM-DD HH:mm:ss");
+            obj.updated_at = moment.tz(job.updated_at, "Africa/Abidjan").tz("Europe/Paris").format("YYYY-MM-DD HH:mm:ss");
+            obj.state = job.state;
+            obj.data_family = ds.family_type;
+            obj.data_type = ds.type;
+            var data_path = ds.name.split('/');
+            obj.data_name = data_path[data_path.length -2] + '/' + data_path[data_path.length -1];
+            to_display.push(obj);
+        }
+    }
+    table_tyr_job_list = $('#table_tyr_job_list').DataTable( {
+        data: to_display,
+        ordering: false,
+        "columns": [
+            {title: "id", "data": "id"},
+            // {title: "instance", "data": "name"},
+            {title: "created_at", "data": "created_at"},
+            {title: "updated_at", "data": "updated_at"},
+            {title: "state", "data": "state"},
+            {title: "family", "data": "data_family"},
+            {title: "type", "data": "data_type"},
+            {title: "name", "data": "data_name"},
+        ]
+    } );
 }
-
-
